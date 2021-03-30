a01306f8c1c0c7e97fcd2af2d54f5675b0f72181
sonohoshi <yd111544@gmail.com>
2021-03-30 10:59:30 +0900

    Update error code
    This StartCoroutine of Start does not works

--- Assets/Scripts/ClientLogic/Player.cs
+++ Assets/Scripts/ClientLogic/Player.cs
@@ -3,13 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PacketNames
+{
+    create,
+    move,
+    attack,
+    damage,
+    ohmygod,
+    lv999boss
+}
+
 public class Player : MonoBehaviour
 {
     private SocketManager socketManager = SocketManager.GetSingleton();
     
-    private bool isMine;
-    private int healthPoint;
-    private float speed;
+    [SerializeField] private bool isMine;
+    [SerializeField] private int healthPoint;
+    [SerializeField] private float speed;
     private bool established;
     
     private long recieveTime, sendTime = 0;
@@ -18,7 +28,11 @@ public class Player : MonoBehaviour
     {
         socketManager.SocketInit("ws://hojoondev.kro.kr:3001", true);
         
-        socketManager.AddOpenEvent((sender, e) => { print("Established!"); });
+        socketManager.AddOpenEvent((sender, e) =>
+        {
+            print("Established!");
+            StartCoroutine(SendPositionInfinitely());
+        });
         
         socketManager.SocketConnect(true);
         socketManager.AddMessageEvent(
@@ -29,27 +43,39 @@ public class Player : MonoBehaviour
             }
         );
         socketManager.AddCloseEvent((sender, e) => { print("Connection Closed"); });
-
-        StartCoroutine(SendPositionInfinitely());
     }
 
     private void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        if (isMine)
+        {
+            MoveWithInput();
+        }
     }
 
     private void SendPositionPacket()
     {
         var pos = transform.position;
-        socketManager.SocketSend($"{gameObject.name},{pos.x},{pos.y}", true, (error) => {sendTime = DateTime.Now.Ticks; });
+        socketManager.SocketSend($"{PacketNames.move:f},{gameObject.name},{pos.x},{pos.y}", true, (error) => {sendTime = DateTime.Now.Ticks; });
     }
 
     private IEnumerator SendPositionInfinitely()
     {
+        Debug.Log("Start coroutine");
         while (true)
         {
             SendPositionPacket();
             yield return new WaitForSeconds(.1f);
         }
+
+        yield return null;
+    }
+
+    private void MoveWithInput()
+    {
+        var horizon = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizon, vertical) * (speed * Time.deltaTime));
     }
 }
