4da92d117efba23bfc5c67cbe5e9278a5e44486a
sonohoshi <yd111544@gmail.com>
2021-04-13 11:01:06 +0900

    Make player factory

--- Assets/Scripts/ClientLogic/Singleton/UIManager.cs
+++ Assets/Scripts/ClientLogic/Singleton/UIManager.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ClientLogic.Singleton
 {
     public class UIManager : Singleton<UIManager>
     {
-        
+        [Header("Make Player")]
+        public InputField nicknameInput;
+
+        public void OnStartingInput()
+        {
+            nicknameInput.onEndEdit.AddListener(OnEnterNickname);
+        }
+
+        private void OnEnterNickname(string name)
+        {
+            Instantiate(Resources.Load("Prefabs/player"), Vector3.zero, Quaternion.identity).name = name;
+            nicknameInput.gameObject.SetActive(false);
+        }
     }
 }
