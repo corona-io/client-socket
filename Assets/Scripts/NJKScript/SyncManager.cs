4c3c93c02760b90d665856b334f35ec2616547b5
6B6390AA <49777427+esrever-NJK@users.noreply.github.com>
2021-04-15 10:05:39 +0900

    Implement Simple Dead Reckoning in SyncManager

--- Assets/Scripts/NJKScript/SyncManager.cs
+++ Assets/Scripts/NJKScript/SyncManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
 
 class InvalidTokenException : Exception
 {
@@ -15,35 +16,42 @@ class InvalidTokenException : Exception
 
 public class SyncManager : MonoBehaviour
 {
-    string message;
+    string? message;
     public GameObject playerPrefab;
+    
     Dictionary<string, GameObject> entityPool;
+    Dictionary<string, int> mutexPool;
+    Dictionary<string, float> lastPacket;
+    Dictionary<string, Vector3> lastPos;
+
     // Start is called before the first frame update
     void Start()
     {
         entityPool = new Dictionary<string, GameObject>();
+        mutexPool = new Dictionary<string, int>();
+        lastPacket = new Dictionary<string, float>();
+        lastPos = new Dictionary<string, Vector3>();
     }
 
     // Update is called once per frame
     void Update()
     {
         message = ConnectionManager.GetRecentMessage();
-        if (message is null) return;
-
-        string[] splitMessage = message.Split(',');
-        IEnumerator routine = splitMessage[0] switch
+        if (!(message is null))
         {
-        "create" => HandleCreateEvent(splitMessage),
-        "move" => HandleMoveEvent(splitMessage),
-        "attack" => HandleAttackEvent(splitMessage),
-        "damage" => HandleDamageEvent(splitMessage),
-        "ohmygod" => HandleDeathEvent(splitMessage),
-        "lv999boss" => HandleLVLUpEvent(splitMessage),
-        _ => throw new NotSupportedException()
-        };
-
-        StartCoroutine(routine);
-        
+            string[] splitMessage = message.Split(',');
+            IEnumerator routine = splitMessage[0] switch
+            {
+                "create" => HandleCreateEvent(splitMessage),
+                "move" => HandleMoveEvent(splitMessage),
+                "attack" => HandleAttackEvent(splitMessage),
+                "damage" => HandleDamageEvent(splitMessage),
+                "ohmygod" => HandleDeathEvent(splitMessage),
+                "lv999boss" => HandleLVLUpEvent(splitMessage),
+                _ => throw new NotSupportedException()
+            };
+            StartCoroutine(routine);
+        }
     }
 
     void CreateEntity(string name, float x, float y) {
@@ -55,6 +63,9 @@ public class SyncManager : MonoBehaviour
                     new Quaternion(0, 0, 0, 0)
                 );
         entityPool[name] = go;
+        mutexPool.Add(name, 0);
+        lastPacket.Add(name, Time.time);
+        lastPos.Add(name, new Vector3(x,y,0));
     }
 
     IEnumerator HandleCreateEvent(string[] tokens) 
@@ -76,16 +87,36 @@ public class SyncManager : MonoBehaviour
     }   
     IEnumerator HandleMoveEvent(string[] tokens) 
     {
-        entityPool.TryGetValue(tokens[1], out GameObject obj);
+        var name = tokens[1];
+
+        entityPool.TryGetValue(name, out GameObject obj);
         if (obj is null)
         {
             // moving an object that doesnt exist???
-            CreateEntity(tokens[1], float.Parse(tokens[2]), float.Parse(tokens[3]));
+            CreateEntity(name, float.Parse(tokens[2]), float.Parse(tokens[3]));
         }
         else 
         {
-            obj.transform.position = new Vector3(float.Parse(tokens[2]), float.Parse(tokens[3]), 0);
-            // TODO: implement dead reckoning
+            mutexPool[name]++;
+            while (mutexPool[name] > 1) yield return new WaitForEndOfFrame();
+            
+            Vector3 newPos = new Vector3(float.Parse(tokens[2]), float.Parse(tokens[3]), 0);
+            Vector3 velocity = (newPos - lastPos[name]) / (Time.time - lastPacket[name]);
+            float lastMovetime;
+            
+            print($"{velocity}");
+            obj.transform.position = lastPos[name] = newPos;
+            lastPacket[name] = lastMovetime = Time.time;
+
+            
+            while (mutexPool[name] < 2 && obj)
+            {
+                obj.transform.position += velocity * (Time.time - lastMovetime);
+                lastMovetime = Time.time;
+                yield return new WaitForEndOfFrame();
+                // TODO: implement dead reckoning
+            }
+            mutexPool[name]--;
         }
 
         yield break;
@@ -94,11 +125,16 @@ public class SyncManager : MonoBehaviour
     IEnumerator HandleDamageEvent(string[] tokens) { yield break; }
     IEnumerator HandleDeathEvent(string[] tokens) 
     {
-        entityPool.TryGetValue(tokens[1], out GameObject obj);
+        var name = tokens[1];
+
+        entityPool.TryGetValue(name, out GameObject obj);
         if (!(obj is null))
         {
             Destroy(obj);
-            entityPool.Remove(tokens[1]);
+            entityPool.Remove(name);
+            mutexPool.Remove(name);
+            lastPacket.Remove(name);
+            lastPos.Remove(name);
         }
         yield break; 
     }
