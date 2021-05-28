dbe962e5f1d5a776b838e4795418bd9e1e9cfe2e
sonohoshi <yd111544@gmail.com>
2021-05-28 09:18:11 +0900

    Create MonoBehaviourExtensions.cs

---
+++ Assets/Scripts/ClientLogic/MonoBehaviourExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace MyExtensions
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static IEnumerator ChangeWithDelay<T>(this T origin, T changeValue, 
+            float delay, Action<T> makeResult)
+        {
+            yield return new WaitForSeconds(delay);
+            makeResult(changeValue);
+        }
+    }
+}
